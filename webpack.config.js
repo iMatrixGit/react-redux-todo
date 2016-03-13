ddbcb921022b3331e35806ded82b76f45d82d8a2
ihadzhiev <vankata_o@abv.bg>
2016-03-13 15:59:09 +0200

    Code formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: [
