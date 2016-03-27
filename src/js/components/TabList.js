c0368524fa1970a9b06dde5ee3e4ec287a8e125c
ihadzhiev <vankata_o@abv.bg>
2016-03-27 17:03:26 +0300

    Add Animations for TabList

--- src/js/components/TabList.js
+++ src/js/components/TabList.js
@@ -2,26 +2,36 @@ import React, { PropTypes } from 'react';
 import { InteractiveTab } from '../containers/containers';
 import Immutable from 'immutable';
 import ImmutablePropTypes from 'react-immutable-proptypes';
+//import { ReactCSSTransitionGroup } from 'react-addons-css-transition-group';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+console.log(ReactCSSTransitionGroup);
 
 export const TabList = ({
     tabs
     }) => {
 
+    let items = tabs.map((tab, index) => (
+        <InteractiveTab
+            key={index}
+            id={tab.get('id')}
+            title={tab.get('title')}
+            content={tab.get('content')}
+            active={tab.get('active')}
+        />
+    ));
+
     return (
 
-        <ul className="tab-list">
-            {
-                tabs.map((tab, index) => (
-                    <InteractiveTab
-                        key={index}
-                        id={tab.get('id')}
-                        title={tab.get('title')}
-                        content={tab.get('content')}
-                        active={tab.get('active')}
-                    />
-                ))
-            }
-        </ul>
+        <div className="tab-list">
+            <ReactCSSTransitionGroup
+                transitionName="example"
+                transitionEnterTimeout={500}
+                transitionLeaveTimeout={300}>
+                {items}
+            </ReactCSSTransitionGroup>
+
+        </div>
     )
 };
 
