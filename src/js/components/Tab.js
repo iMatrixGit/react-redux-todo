70b1924527d525787d9d9a92e325ab8556affdcb
ihadzhiev <vankata_o@abv.bg>
2016-03-30 21:51:17 +0300

    Optimize Tab component rendering

--- src/js/components/Tab.js
+++ src/js/components/Tab.js
@@ -1,30 +1,37 @@
 import React, { PropTypes } from 'react';
 import { TabHeading } from './TabHeading';
 import { RemTabBtn } from './RemTabBtn';
+import shallowCompare from 'react-addons-shallow-compare';
+import _ from 'underscore';
 
-export const Tab = ({
-    id,
-    title,
-    content,
-    active,
-    toggleTab,
-    removeTab
-    }) => {
+export class Tab extends React.Component {
 
-    let tabContent = active ? <div className="tab-content">{content}</div> : '';
+    shouldComponentUpdate(nextProps){
+        return !_.isEqual(this.props, nextProps);
+    }
 
-    return (
-        <div className="tab">
-            <TabHeading title={title} onClick={() => toggleTab(id)} />
-            <RemTabBtn onClick={() => removeTab(id)} />
-            {tabContent}
-        </div>
-    );
-};
+    render(){
+
+        const { id, title, content, active, toggleTab, removeTab } = this.props;
+        let tabContent = active ? <div className="tab-content">{content}</div> : '';
+
+        return (
+            <div className="tab">
+                <TabHeading title={title} onClick={() => toggleTab(id)} />
+                <RemTabBtn onClick={() => removeTab(id)} />
+                {tabContent}
+            </div>
+        );
+    }
+}
 
 Tab.propTypes = {
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
     active: PropTypes.bool.isRequired
+};
+
+Tab.contextTypes = {
+    store: PropTypes.object.isRequired
 };
