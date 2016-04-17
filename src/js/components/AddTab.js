c299492888d526d7657c294728933b48487314e1
ihadzhiev <vankata_o@abv.bg>
2016-04-17 18:54:40 +0300

    Overide AddTab component with material-ui

--- src/js/components/AddTab.js
+++ src/js/components/AddTab.js
@@ -1,42 +1,67 @@
 import React, { PropTypes } from 'react';
 import { pure } from 'recompose'
+import TextField from 'material-ui/lib/text-field';
+import RaisedButton from 'material-ui/lib/raised-button';
 
-const AddTab = ({
-    addTab
-    }) => {
+class AddTab extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            titleText: '',
+            contentText: ''
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleContentChange = this.handleContentChange.bind(this);
+    }
 
-    let inputTitle, inputContent;
+    handleClick(e){
+        const { addTab } = this.props;
+        const {titleText, contentText} = this.state;
 
-    return (
+        addTab(titleText, contentText);
 
-        <div className="add-tab-menu">
-            <label htmlFor="tab_title">Tab title:</label>
-            <input type="text"
-                   id="tab_title"
-                   className="form-input"
-                   placeholder="Enter tab title"
-                   ref={ node => inputTitle = node }
-            />
-            <label htmlFor="tab_content">Tab content:</label>
-            <input type="text"
-                   id="tab_content"
-                   className="form-input"
-                   placeholder="Enter tab content"
-                   ref={ node => inputContent = node }
-            />
-            <button
-                type="button"
-                className="form-submit"
-                onClick={() => {
-                addTab(inputTitle.value, inputContent.value);
-                inputTitle.value = '';
-                inputContent.value = '';
-            }}>
-                Add Tab
-            </button>
-        </div>
-    )
+        this.setState({
+            titleText: '',
+            contentText: ''
+        })
+    }
 
-};
+    handleTitleChange(e){
+        this.setState({
+            titleText: e.target.value
+        });
+    }
+
+    handleContentChange(e){
+        this.setState({
+            contentText: e.target.value
+        });
+    }
+
+    render(){
+        const { titleText, contentText } = this.state;
+
+        return (
+            <div className="add-tab-menu">
+                <TextField
+                    hintText="Enter tab title"
+                    value={titleText}
+                    onChange={this.handleTitleChange}
+                />
+                <TextField
+                    hintText="Enter tab title"
+                    value={contentText}
+                    onChange={this.handleContentChange}
+                />
+                <RaisedButton
+                    label="Add tab"
+                    secondary={true}
+                    onMouseUp={this.handleClick}
+                />
+            </div>
+        )
+    }
+}
 
 export default pure(AddTab);
